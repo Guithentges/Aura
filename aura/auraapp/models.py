1ddeb65e91fb6e2e2342e44bae0f1238cfa95256
Guithentges <guithentges@gmail.com>
2025-05-15 20:14:27 -0300

    criando o model do produto e fazendo a migration

--- aura/auraapp/models.py
+++ aura/auraapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome

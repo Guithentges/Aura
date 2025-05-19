d562f8ec1c456f4cb82c9af9c29fce1711694c9b
Guithentges <guithentges@gmail.com>
2025-05-18 21:19:37 -0300

    Criando todos os models do banco

--- aura/auraapp/admin.py
+++ aura/auraapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from auraapp.models import Produto
+from auraapp.models import Produto, Categoria, Cliente, Pedido
 
 
 #login admin
 #senha admin
 
 admin.site.register(Produto)
+admin.site.register(Cliente)
+admin.site.register(Categoria)
+admin.site.register(Pedido)
--- aura/auraapp/models.py
+++ aura/auraapp/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nome
+
+
 class Produto(models.Model):
     nome = models.CharField(max_length=150)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
-    categoria = models.CharField(max_length=150)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='static/product/', default='./static/blazer.png')
 
     def __str__(self):
         return self.nome
+    
+class Pedido(models.Model):
+    Cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.produto

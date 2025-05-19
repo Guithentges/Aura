36cff9c26c8f8018348b2472bde63f076eafa071
Guithentges <guithentges@gmail.com>
2025-05-19 00:18:10 -0300

    Avançando na estilização da home page

--- aura/auraapp/models.py
+++ aura/auraapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=300, default='Descrição')
     def __str__ (self):
         return self.nome
 
@@ -17,7 +18,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=150)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to='static/product/', default='./static/blazer.png')
+    imagem = models.ImageField(upload_to='./static/', default='./static/blazer.png')
 
     def __str__(self):
         return self.nome
--- aura/auraapp/views.py
+++ aura/auraapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Produto, Categoria
 
 # Create your views here.
 def index(request):
-    return render(request, 'auraapp/index.html')
+
+    produtos = Produto.objects.all()
+    categorias = Categoria.objects.all()
+
+    return render(request, 'auraapp/index.html', {'produtos':produtos, 'categorias':categorias})
